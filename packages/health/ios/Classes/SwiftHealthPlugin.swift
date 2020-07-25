c2c544432a0b2388ae82c0c7b74410fcf0778519
Nikolay Burykin <nikolay.burykin@yandex.ru>
2020-07-25 13:59:42 +0300

    Fix statistic query

--- packages/health/ios/Classes/SwiftHealthPlugin.swift
+++ packages/health/ios/Classes/SwiftHealthPlugin.swift
@@ -93,7 +93,7 @@ public class SwiftHealthPlugin: NSObject, FlutterPlugin {
         let dateTo = Date(timeIntervalSince1970: endDate.doubleValue / 1000)
 
         let dataType = dataTypeLookUp(key: dataTypeKey)
-        let predicate = HKQuery.predicateForSamples(withStart: dateFrom, end: dateTo, options: .strictStartDate)
+        let predicate = HKStatisticsQuery.predicateForSamples(withStart: dateFrom, end: dateTo, options: .strictStartDate)
         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)
 
         let query = HKSampleQuery(sampleType: dataType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) {
